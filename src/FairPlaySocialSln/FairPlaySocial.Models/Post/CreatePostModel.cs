126be77d2f385730a859e78825d53248d2c602cf
Eduardo Fonseca <efonsecab@gmail.com>
2022-11-26 12:49:38 -0600

    Adding validation to allow tags to only one word

--- src/FairPlaySocialSln/FairPlaySocial.Models/Post/CreatePostModel.cs
+++ src/FairPlaySocialSln/FairPlaySocial.Models/Post/CreatePostModel.cs
@@ -12,6 +12,9 @@ namespace FairPlaySocial.Models.Post
 {
     public class CreatePostModel
     {
+        private const string PATTERN_JUST_ONE_WORD = @"^\b[a-zA-Z0-9_]+\b$";
+        private const string INVALID_TAG_ERROR_MESSAGE = "Tags can only contain one word composed of only letters and numbers";
+
         [Required]
         [StringLength(500)]
         [ProhibitHashTags(ErrorMessage = "Text cannot contain HashTags")]
@@ -21,10 +24,13 @@ namespace FairPlaySocial.Models.Post
         [ValidateComplexType]
         public CreatePhotoModel? Photo { get; set; }
         [Required]
+        [RegularExpression(PATTERN_JUST_ONE_WORD, ErrorMessage = INVALID_TAG_ERROR_MESSAGE)]
         public string? Tag1 { get; set; }
         [Required]
+        [RegularExpression(PATTERN_JUST_ONE_WORD, ErrorMessage = INVALID_TAG_ERROR_MESSAGE)]
         public string? Tag2 { get; set; }
         [Required]
+        [RegularExpression(PATTERN_JUST_ONE_WORD, ErrorMessage = INVALID_TAG_ERROR_MESSAGE)]
         public string? Tag3 { get; set; }
     }
 }
