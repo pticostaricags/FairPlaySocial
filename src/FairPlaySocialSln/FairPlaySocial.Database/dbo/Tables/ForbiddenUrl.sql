d77c17412d8cd306ee62acbb8acce168d011ca92
Eduardo Fonseca <efonsecab@gmail.com>
2022-11-28 07:36:48 -0600

    Adding base table for forbidden urls

---
+++ src/FairPlaySocialSln/FairPlaySocial.Database/dbo/Tables/ForbiddenUrl.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ForbiddenUrl]
+(
+	[ForbiddenUrlId] BIGINT NOT NULL CONSTRAINT PK_ForbiddenUrl PRIMARY KEY IDENTITY, 
+    [Url] NVARCHAR(1000) NOT NULL
+)
+
+GO
+
+CREATE UNIQUE INDEX [UI_ForbiddenUrl_Url] ON [dbo].[ForbiddenUrl] ([Url])
