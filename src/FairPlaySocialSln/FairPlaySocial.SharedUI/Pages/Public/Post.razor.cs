2db69aeb4dfdbb90f814f81c2ba17439ecddff2c
Eduardo Fonseca <efonsecab@gmail.com>
2022-12-19 17:44:46 -0600

    Creating the PostComponent

---
+++ src/FairPlaySocialSln/FairPlaySocial.SharedUI/Pages/Public/Post.razor.cs
@@ -0,0 +1,18 @@
+﻿using FairPlaySocial.Models.Post;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairPlaySocial.SharedUI.Pages.Public
+{
+    public partial class Post
+    {
+        [Parameter]
+        public long? PostId { get; set; }
+        private bool IsBusy { get; set; } = false;
+        private PostModel? PostModel { get; set; }
+    }
+}
