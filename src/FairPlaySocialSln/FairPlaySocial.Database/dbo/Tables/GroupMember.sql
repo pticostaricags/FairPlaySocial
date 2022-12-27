921f26d75a6b28d73856a7cd80213a7b57d8e5b1
Tarun Samanta <tarunsamanta77@gmail.com>
2022-12-27 18:21:28 +0530

    made required changes

--- src/FairPlaySocialSln/FairPlaySocial.Database/dbo/Tables/GroupMember.sql
+++ src/FairPlaySocialSln/FairPlaySocial.Database/dbo/Tables/GroupMember.sql
@@ -1,9 +1,14 @@
-CREATE TABLE [dbo].[Group]
+CREATE TABLE [dbo].[GroupMember]
 (
-    [GroupId] BIGINT NOT NULL CONSTRAINT PK_Group_M PRIMARY KEY IDENTITY,
+    [GroupMemberId] BIGINT NOT NULL CONSTRAINT PK_GroupMember PRIMARY KEY IDENTITY,
     [MemberApplicationUserId]  BIGINT NOT NULL,
     [RowCreationDateTime] DATETIMEOFFSET NOT NULL, 
     [RowCreationUser] NVARCHAR(256) NOT NULL,
     [SourceApplication] NVARCHAR(250) NOT NULL, 
-    [OriginatorIPAddress] NVARCHAR(100) NOT NULL
-);
+    [OriginatorIPAddress] NVARCHAR(100) NOT NULL,
+    CONSTRAINT [FK_GroupMember_ApplicationUser] FOREIGN KEY ([MemberApplicationUserId]) REFERENCES [ApplicationUser]([ApplicationUserId])
+)
+
+GO
+
+CREATE UNIQUE INDEX [UI_GroupMember_GroupMemberId_MemberApplicationUserId] ON [dbo].[GroupMember] ([GroupMemberId], [MemberApplicationUserId])
