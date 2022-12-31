4cbfde55aa56721a40de14d3c5f3edde2c149cb1
Eduardo Fonseca <efonsecab@gmail.com>
2022-12-31 06:52:22 -0600

    Integrating Azure Translator. Adding base for Culture Selection in WebAssembly app

---
+++ src/FairPlaySocialSln/FairPlaySocial/Client/Shared/CultureSelector.razor.cs
@@ -0,0 +1,73 @@
+﻿using FairPlaySocial.ClientServices;
+using FairPlaySocial.Common.CustomAttributes.Localization;
+using FairPlaySocial.Common.Interfaces.Services;
+using FairPlaySocial.Models.Localization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using Microsoft.JSInterop;
+using System.Globalization;
+
+namespace FairPlaySocial.Client.Shared
+{
+    public partial class CultureSelector
+    {
+        [Inject]
+        public NavigationManager? NavManager { get; set; }
+
+        [Inject]
+        public IJSRuntime? JSRuntime { get; set; }
+        [Inject]
+        private IStringLocalizer<CultureSelector>? Localizer { get; set; }
+        [Inject]
+        private LocalizationClientService? LocalizationClientService { get; set; }
+        [Inject]
+        private IToastService? ToastService { get; set; }
+        private CultureModel[]? CultureModels { get; set; }
+        private CultureInfo[]? SupportedCultures { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                this.CultureModels = await this.LocalizationClientService!
+                    .GetSupportedCulturesAsync();
+                this.SupportedCultures = CultureModels!
+                    .Select(p => CultureInfo.GetCultureInfo(p.Name!)).ToArray();
+            }
+            catch (Exception ex)
+            {
+                await this.ToastService!
+                    .ShowErrorMessageAsync(ex.Message,CancellationToken.None);
+            }
+        }
+
+        CultureInfo Culture
+        {
+            get => CultureInfo.CurrentCulture;
+            set
+            {
+                if (CultureInfo.CurrentCulture != value)
+                {
+                    var js = (IJSInProcessRuntime)JSRuntime!;
+                    js.InvokeVoid("blazorCulture.set", value.Name);
+
+                    NavManager!.NavigateTo(NavManager.Uri, forceLoad: true);
+                }
+            }
+        }
+
+        public string GetDisplayName(CultureInfo culture)
+        {
+            return CultureModels!.Single(p => p.Name == culture.Name).DisplayName!;
+        }
+
+        #region Resource Keys
+        [ResourceKey(defaultValue: "Language")]
+        public const string LanguageTextKey = "LanguageText";
+        [ResourceKey(defaultValue: "Disclaimer: Translations are automatically generated " +
+            "using Microsoft Azure Translator. As with any automated " +
+            "translation technology the text may not be perfect.")]
+        public const string TranslationsDisclaimerTextKey = "TranslationsDisclaimerText";
+        #endregion Resource Keys
+    }
+}
---
+++ src/FairPlaySocialSln/FairPlaySocial/Server/AutoMapperProfiles/ResourceProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using FairPlaySocial.DataAccess.Models;
+using FairPlaySocial.Models.Localization;
+
+namespace FairPlaySocial.Server.AutoMapperProfiles
+{
+    public class ResourceProfile : Profile
+    {
+        public ResourceProfile() 
+        {
+            this.CreateMap<Resource, ResourceModel>().AfterMap((source, dest) =>
+            {
+                if (source.Culture != null)
+                {
+                    dest.CultureName = source.Culture.Name;
+                }
+            });
+        }
+    }
+}
