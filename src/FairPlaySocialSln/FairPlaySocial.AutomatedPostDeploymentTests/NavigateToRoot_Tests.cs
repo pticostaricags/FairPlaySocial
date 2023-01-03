9e50c611dc32398e2b16723f1063147854e82545
Eduardo Fonseca <efonsecab@gmail.com>
2023-01-03 13:45:05 -0600

    Adding post deployment tests

---
+++ src/FairPlaySocialSln/FairPlaySocial.AutomatedPostDeploymentTests/NavigateToRoot_Tests.cs
@@ -0,0 +1,29 @@
+using Microsoft.Playwright;
+
+namespace FairPlaySocial.AutomatedPostDeploymentTests
+{
+    [TestClass]
+    public class NavigateToRoot_Tests : PostDeploymentTestsBase
+    {
+        [TestMethod]
+        public async Task Test_NavigateToHomeAsync()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false,
+            });
+            var context = await browser.NewContextAsync();
+
+            var page = await context.NewPageAsync();
+
+            await page.GotoAsync(PostDeploymentTestsBase.BaseUrl);
+
+            await page.GetByText("FairPlaySocial (Beta)").ClickAsync();
+
+            await page.GetByText("Welcome to FairPlaySocial. The Multi-platform system to share your thoughts.").ClickAsync();
+
+            await page.Locator("a").First.ClickAsync();
+        }
+    }
+}
---
+++ src/FairPlaySocialSln/FairPlaySocial.AutomatedPostDeploymentTests/PostDeploymentTestsBase.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairPlaySocial.AutomatedPostDeploymentTests
+{
+    public class PostDeploymentTestsBase
+    {
+        protected const string BaseUrl = "https://fairplaysocialdev.azurewebsites.net/";
+        public TestAzureAdB2CAuthConfiguration? TestAzureAdB2CAuthConfiguration { get; }
+        public PostDeploymentTestsBase()
+        {
+            ConfigurationBuilder configurationBuilder = new();
+#if DEBUG
+            configurationBuilder.AddUserSecrets<PostDeploymentTestsBase>();
+#else
+            var appSettingsBase64Content = Environment.GetEnvironmentVariable("AppSettingsContent")!;
+            var appSettingsBytes = Convert.FromBase64String(appSettingsBase64Content);
+            MemoryStream memoryStream = new MemoryStream(appSettingsBytes);
+            configurationBuilder.AddJsonStream(memoryStream);
+#endif
+            IConfiguration configuration = configurationBuilder.Build();
+            this.TestAzureAdB2CAuthConfiguration = configuration
+                .GetSection(nameof(TestAzureAdB2CAuthConfiguration))
+                .Get<TestAzureAdB2CAuthConfiguration>();
+        }
+    }
+
+    public class TestAzureAdB2CAuthConfiguration
+    {
+        public string? TokenUrl { get; set; }
+        public string? UserRoleUsername { get; set; }
+        public string? UserRolePassword { get; set; }
+        public string? AdminRoleUsername { get; set; }
+        public string? AdminRolePassword { get; set; }
+        public string? ApplicationId { get; set; }
+        public string? AzureAdUserObjectId { get; set; }
+    }
+}
