90bfefeaa285b63ece5d83df096711585cbdb28f
Eduardo Fonseca <efonsecab@gmail.com>
2023-01-03 19:35:45 -0600

    Adding login tests

--- src/FairPlaySocialSln/FairPlaySocial.AutomatedPostDeploymentTests/Chromium/NavigateToRoot_Tests.cs
+++ src/FairPlaySocialSln/FairPlaySocial.AutomatedPostDeploymentTests/Chromium/NavigateToRoot_Tests.cs
@@ -1,10 +1,41 @@
-using Microsoft.Playwright;
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.MSTest;
 
 namespace FairPlaySocial.AutomatedPostDeploymentTests.Chromium
 {
     [TestClass]
     public class NavigateToRoot_Tests : PostDeploymentTestsBase
     {
+        [TestMethod]
+        public async Task Test_LoginFlowAsync()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false,
+            });
+            var context = await browser.NewContextAsync();
+
+            var page = await context.NewPageAsync();
+
+            await page.GotoAsync(BaseUrl);
+
+            await page.GetByRole(AriaRole.Button, new() { NameString = "Log In" }).ClickAsync();
+
+            await page.GetByPlaceholder("Email Address").ClickAsync();
+
+            await page.GetByPlaceholder("Email Address").FillAsync(TestAzureAdB2CAuthConfiguration!.UserRoleUsername!);
+
+            await page.GetByPlaceholder("Email Address").PressAsync("Tab");
+
+            await page.GetByPlaceholder("Password").FillAsync(TestAzureAdB2CAuthConfiguration!.UserRolePassword!);
+
+            await page.GetByRole(AriaRole.Button, new() { NameString = "Sign in" }).ClickAsync();
+            await page.GetByRole(AriaRole.Button, new() { Name = "Log Out"}).ClickAsync();
+            var loggedoutIndicatorLocator = page.GetByRole(AriaRole.Heading, new() { NameString = "Log Out Succeeded" });;
+            await Assertions.Expect(loggedoutIndicatorLocator).ToHaveTextAsync("Log Out Succeeded");
+        }
+
         [TestMethod]
         public async Task Test_NavigateToHomeAsync()
         {
--- src/FairPlaySocialSln/FairPlaySocial.AutomatedPostDeploymentTests/Firefox/NavigateToRoot_Tests.cs
+++ src/FairPlaySocialSln/FairPlaySocial.AutomatedPostDeploymentTests/Firefox/NavigateToRoot_Tests.cs
@@ -6,6 +6,36 @@ namespace FairPlaySocial.AutomatedPostDeploymentTests.Firefox
     public class NavigateToRoot_Tests : PostDeploymentTestsBase
     {
         [TestMethod]
+        public async Task Test_LoginFlowAsync()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Firefox.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false,
+            });
+            var context = await browser.NewContextAsync();
+
+            var page = await context.NewPageAsync();
+
+            await page.GotoAsync(BaseUrl);
+
+            await page.GetByRole(AriaRole.Button, new() { NameString = "Log In" }).ClickAsync();
+
+            await page.GetByPlaceholder("Email Address").ClickAsync();
+
+            await page.GetByPlaceholder("Email Address").FillAsync(TestAzureAdB2CAuthConfiguration!.UserRoleUsername!);
+
+            await page.GetByPlaceholder("Email Address").PressAsync("Tab");
+
+            await page.GetByPlaceholder("Password").FillAsync(TestAzureAdB2CAuthConfiguration!.UserRolePassword!);
+
+            await page.GetByRole(AriaRole.Button, new() { NameString = "Sign in" }).ClickAsync();
+            await page.GetByRole(AriaRole.Button, new() { Name = "Log Out" }).ClickAsync();
+            var loggedoutIndicatorLocator = page.GetByRole(AriaRole.Heading, new() { NameString = "Log Out Succeeded" }); ;
+            await Assertions.Expect(loggedoutIndicatorLocator).ToHaveTextAsync("Log Out Succeeded");
+        }
+
+        [TestMethod]
         public async Task Test_NavigateToHomeAsync()
         {
             using var playwright = await Playwright.CreateAsync();
