48d54f98316a9665fbfcdc95143c6d521389b1e1
Eduardo Fonseca <efonsecab@gmail.com>
2023-01-06 07:41:51 -0600

    Adding localization for CreatePostModel

--- src/FairPlaySocialSln/FairPlaySocial.Models/Post/CreatePostModel.cs
+++ src/FairPlaySocialSln/FairPlaySocial.Models/Post/CreatePostModel.cs
@@ -1,42 +1,52 @@
 ﻿using FairPlaySocial.Common.CustomAttributes.Localization;
 using FairPlaySocial.CustomValidation.CustomValidationAttributes;
 using FairPlaySocial.Models.Photo;
-using FairPlaySocial.Models.PostTag;
 using Microsoft.Extensions.Localization;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FairPlaySocial.Models.Post
 {
     public class CreatePostModel
     {
         private const string PATTERN_JUST_ONE_WORD = @"^\b[a-zA-Z0-9_]+\b$";
-        private const string INVALID_TAG_ERROR_MESSAGE = "Tags can only contain one word composed of only letters and numbers";
 
         [Required(ErrorMessageResourceName = nameof(CreatePostModelLocalizer.TextRequired),
-            ErrorMessageResourceType =typeof(CreatePostModelLocalizer))]
-        [StringLength(500)]
-        [ProhibitHashTags(ErrorMessage = "Text cannot contain HashTags")]
-        [ProhibitUrls(ErrorMessage = "Text cannot contain Urls")]
+            ErrorMessageResourceType = typeof(CreatePostModelLocalizer))]
+        [StringLength(500,
+            ErrorMessageResourceName = nameof(CreatePostModelLocalizer.TextStringLength),
+            ErrorMessageResourceType = typeof(CreatePostModelLocalizer))]
+        [ProhibitHashTags(ErrorMessage = "Text cannot contain HashTags",
+            ErrorMessageResourceName = nameof(CreatePostModelLocalizer.TextProhibitHashTags),
+            ErrorMessageResourceType = typeof(CreatePostModelLocalizer))]
+        [ProhibitUrls(ErrorMessage = "Text cannot contain Urls",
+            ErrorMessageResourceName = nameof(CreatePostModelLocalizer.TextProhibitUrls),
+            ErrorMessageResourceType = typeof(CreatePostModelLocalizer))]
         public string? Text { get; set; }
         [Required]
         [ValidateComplexType]
         public CreatePhotoModel? Photo { get; set; }
-        [Required]
-        [RegularExpression(PATTERN_JUST_ONE_WORD, ErrorMessage = INVALID_TAG_ERROR_MESSAGE)]
+        [Required(ErrorMessageResourceName =nameof(CreatePostModelLocalizer.TagRequired),
+            ErrorMessageResourceType =typeof(CreatePostModelLocalizer))]
+        [RegularExpression(PATTERN_JUST_ONE_WORD,
+            ErrorMessageResourceName = nameof(CreatePostModelLocalizer.InvalidTagPattern),
+            ErrorMessageResourceType = typeof(CreatePostModelLocalizer))]
         public string? Tag1 { get; set; }
-        [Required]
-        [RegularExpression(PATTERN_JUST_ONE_WORD, ErrorMessage = INVALID_TAG_ERROR_MESSAGE)]
+        [Required(ErrorMessageResourceName = nameof(CreatePostModelLocalizer.TagRequired),
+            ErrorMessageResourceType = typeof(CreatePostModelLocalizer))]
+        [RegularExpression(PATTERN_JUST_ONE_WORD,
+            ErrorMessageResourceName = nameof(CreatePostModelLocalizer.InvalidTagPattern),
+            ErrorMessageResourceType = typeof(CreatePostModelLocalizer))]
         public string? Tag2 { get; set; }
-        [Required]
-        [RegularExpression(PATTERN_JUST_ONE_WORD, ErrorMessage = INVALID_TAG_ERROR_MESSAGE)]
+        [Required(
+            ErrorMessageResourceName = nameof(CreatePostModelLocalizer.TagRequired),
+            ErrorMessageResourceType = typeof(CreatePostModelLocalizer))]
+        [RegularExpression(PATTERN_JUST_ONE_WORD,
+            ErrorMessageResourceName = nameof(CreatePostModelLocalizer.InvalidTagPattern),
+            ErrorMessageResourceType = typeof(CreatePostModelLocalizer))]
         public string? Tag3 { get; set; }
-        [Url]
+        [Url(
+            ErrorMessageResourceName = nameof(CreatePostModelLocalizer.UrlValidFormatRequired),
+            ErrorMessageResourceType = typeof(CreatePostModelLocalizer))]
         public string? Url { get; set; }
         [Required]
         [Range(1, 2)]
@@ -51,10 +61,27 @@ namespace FairPlaySocial.Models.Post
     {
         public static IStringLocalizer<CreatePostModelLocalizer>? Localizer { get; set; }
         public static string TextRequired => Localizer![TextRequiredTextKey];
-
+        public static string TextStringLength => Localizer![TextStringLengthTextKey];
+        public static string TextProhibitHashTags => Localizer![TextProhibitHashTagsTextKey];
+        public static string InvalidTagPattern => Localizer![InvalidTagPatternTextKey];
+        public static string TextProhibitUrls => Localizer![TextProhibitUrlsTextKey];
+        public static string TagRequired => Localizer![TagRequiredTextKey];
+        public static string UrlValidFormatRequired => Localizer![UrlValidFormatRequiredTextKey];
         #region Resource Keys
-        [ResourceKey(defaultValue:"Text is required")]
+        [ResourceKey(defaultValue: "Text is required")]
         public const string TextRequiredTextKey = "TextRequiredText";
+        [ResourceKey(defaultValue: "Text can have a maximum of {1} characters")]
+        public const string TextStringLengthTextKey = "TextStringLengthText";
+        [ResourceKey(defaultValue: "Text cannot have hashtags")]
+        public const string TextProhibitHashTagsTextKey = "TextProhibitHashTagsText";
+        [ResourceKey(defaultValue: "Text cannot have hashtags")]
+        public const string TextProhibitUrlsTextKey = "TextProhibitUrlsText";
+        [ResourceKey(defaultValue: "Tags can only contain one word composed of only letters and numbers")]
+        public const string InvalidTagPatternTextKey = "InvalidTagPatternText";
+        [ResourceKey(defaultValue: "Tag is required")]
+        public const string TagRequiredTextKey = "TagRequiredText";
+        [ResourceKey(defaultValue: "Urls must have a valid format")]
+        public const string UrlValidFormatRequiredTextKey = "UrlValidFormatRequiredText";
         #endregion Resource Keys
     }
 }
