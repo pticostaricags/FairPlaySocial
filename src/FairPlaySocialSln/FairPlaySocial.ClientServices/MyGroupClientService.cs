abe02cace83978554d332119bc8e43036dd3fa86
Eduardo Fonseca <efonsecab@gmail.com>
2023-01-07 05:52:19 -0600

    Adding MyGroupClientService

---
+++ src/FairPlaySocialSln/FairPlaySocial.ClientServices/MyGroupClientService.cs
@@ -0,0 +1,31 @@
+﻿using FairPlaySocial.Models.Extensions;
+using FairPlaySocial.Models.Group;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairPlaySocial.ClientServices
+{
+    public class MyGroupClientService
+    {
+        private readonly HttpClientService httpClientService;
+
+        public MyGroupClientService(HttpClientService httpClientService)
+        {
+            this.httpClientService = httpClientService;
+        }
+
+        public async Task<GroupModel?> CreateMyGroupAsync(CreateGroupModel createGroupModel, CancellationToken cancellationToken)
+        {
+            var requestUrl = "api/MyGroup/CreateMyGroup";
+            var authorizedHttpClient = this.httpClientService.CreateAuthorizedClient();
+            var response = await authorizedHttpClient.PostAsJsonAsync(requestUrl, createGroupModel, cancellationToken:cancellationToken);
+            await response.CustomEnsureSuccessStatusCodeAsync();
+            var result = await response.Content.ReadFromJsonAsync<GroupModel>();
+            return result;
+        }
+    }
+}
