211e6561c19133696b5c10dda1144a2bee1b5676
Eduardo Fonseca <efonsecab@gmail.com>
2023-01-07 05:41:19 -0600

    Creating the MyGroupController

---
+++ src/FairPlaySocialSln/FairPlaySocial/Server/AutoMapperProfiles/GroupProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using FairPlaySocial.DataAccess.Models;
+using FairPlaySocial.Models.Group;
+
+namespace FairPlaySocial.Server.AutoMapperProfiles
+{
+    public class GroupProfile : Profile
+    {
+        public GroupProfile() 
+        {
+            CreateMap<Group, GroupModel>();
+            CreateMap<GroupModel, Group>();
+            CreateMap<CreateGroupModel, Group>();
+        }
+    }
+}
---
+++ src/FairPlaySocialSln/FairPlaySocial/Server/Controllers/MyGroupController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using FairPlaySocial.Common.CustomExceptions;
+using FairPlaySocial.Common.Global;
+using FairPlaySocial.Common.Interfaces;
+using FairPlaySocial.DataAccess.Models;
+using FairPlaySocial.Models.Group;
+using FairPlaySocial.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FairPlaySocial.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = Constants.Roles.User)]
+    public class MyGroupController : ControllerBase
+    {
+        private readonly ICurrentUserProvider currentUserProvider;
+        private readonly IMapper mapper;
+        private readonly GroupService groupService;
+
+        public MyGroupController(ICurrentUserProvider currentUserProvider, IMapper mapper, GroupService groupService)
+        {
+            this.currentUserProvider = currentUserProvider;
+            this.mapper = mapper;
+            this.groupService = groupService;
+        }
+
+        [HttpPost("action")]
+        public async Task<GroupModel?> CreateMyGroupAsync(CreateGroupModel createGroupModel,
+            CancellationToken cancellationToken)
+        {
+            if (await this.groupService.GetAllGroup(trackEntities: false,
+                cancellationToken: cancellationToken)
+                .Where(p => p.Name == createGroupModel.Name).AnyAsync(cancellationToken: cancellationToken))
+            {
+                throw new CustomValidationException($"Unable to create group {createGroupModel.Name}. That name is already being used");
+            }
+            Group groupEntity = this.mapper.Map<CreateGroupModel, Group>(createGroupModel);
+            groupEntity = await this.groupService.CreateGroupAsync(groupEntity, cancellationToken);
+            var result = this.mapper.Map<Group, GroupModel>(groupEntity);
+            return result;
+        }
+    }
+}
