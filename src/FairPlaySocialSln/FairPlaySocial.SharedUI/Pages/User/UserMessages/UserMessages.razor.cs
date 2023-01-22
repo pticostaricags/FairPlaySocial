71e8fe6ad3c1aa111d478e803eb96767f36744f7
Eduardo Fonseca <efonsecab@gmail.com>
2023-01-22 06:47:43 -0600

    Adding missing loading flags

--- src/FairPlaySocialSln/FairPlaySocial.SharedUI/Pages/User/UserMessages/UserMessages.razor.cs
+++ src/FairPlaySocialSln/FairPlaySocial.SharedUI/Pages/User/UserMessages/UserMessages.razor.cs
@@ -1,18 +1,12 @@
 ﻿using FairPlaySocial.ClientServices;
 using FairPlaySocial.Common.Global;
 using FairPlaySocial.Common.Interfaces.Services;
-using FairPlaySocial.Models.ApplicationUser;
 using FairPlaySocial.Models.Pagination;
 using FairPlaySocial.Models.Search;
 using FairPlaySocial.Models.UserMessage;
 using FairPlaySocial.Models.UserProfile;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FairPlaySocial.SharedUI.Pages.User.UserMessages
 {
@@ -28,8 +22,6 @@ namespace FairPlaySocial.SharedUI.Pages.User.UserMessages
         private IToastService? ToastService { get; set; }
         private bool IsBusy { get; set; } = false;
         private UserMessageModel[]? UserMessageModels { get; set; }
-        private UserProfileModel[]? UserProfiles { get; set; }
-        private bool ShowSearchModal { get; set; }
         private PagedItems<UserProfileModel>? FilteredUserProfiles { get; set; }
         private UserProfileModel? SelectedUserProfile { get; set; } = new();
 
@@ -38,39 +30,55 @@ namespace FairPlaySocial.SharedUI.Pages.User.UserMessages
             SearchType = SearchType.UserProfiles
         };
 
-        private void ShowSearch()
-        {
-            this.ShowSearchModal = true;
-        }
-
-        private void HideSearch()
-        {
-            this.ShowSearchModal = false;
-        }
-
         private async Task SearchUserAsync()
         {
-            this.FilteredUserProfiles = await this.SearchClientService!
+            try
+            {
+                this.IsBusy = true;
+                this.FilteredUserProfiles = await this.SearchClientService!
                 .SearchUserProfilesAsync(new Models.Pagination.PageRequestModel()
                 {
                     PageNumber = 1
                 },
                 this.searchModel!.SearchTerm!,
                 base.CancellationToken);
+            }
+            catch (Exception ex)
+            {
+                await this.ToastService!
+                    .ShowErrorMessageAsync(ex.Message, base.CancellationToken);
+            }
+            finally
+            {
+                this.IsBusy = false;
+            }
 
             StateHasChanged();
         }
 
         private async Task OnSelectedUserProfileChangedAsync(long? selectedApplicationUserId)
         {
-            await Task.Yield();
-            this.SelectedUserProfile!.ApplicationUserId = null;
-            this.UserMessageModels = null;
-            StateHasChanged();
-            this.SelectedUserProfile!.ApplicationUserId = selectedApplicationUserId;
-            this.UserMessageModels = await this.UserMessageClientService!
-                .GetMyMessagesWithUserAsync(selectedApplicationUserId!.Value, base.CancellationToken);
-            StateHasChanged();
+            try
+            {
+                this.IsBusy = true;
+                await Task.Yield();
+                this.SelectedUserProfile!.ApplicationUserId = null;
+                this.UserMessageModels = null;
+                StateHasChanged();
+                this.SelectedUserProfile!.ApplicationUserId = selectedApplicationUserId;
+                this.UserMessageModels = await this.UserMessageClientService!
+                    .GetMyMessagesWithUserAsync(selectedApplicationUserId!.Value, base.CancellationToken);
+                StateHasChanged();
+            }
+            catch (Exception ex)
+            {
+                await this.ToastService!
+                    .ShowErrorMessageAsync(ex.Message, base.CancellationToken);
+            }
+            finally
+            {
+                this.IsBusy = false;
+            }
         }
 
         private async Task OnMessageSentAsync()
